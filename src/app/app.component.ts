95d1fe17bff7e56b36e36cc531de7e89d5b35599
TheMaxium69 <mxm.tournier@gmail.com>
2024-09-02 10:15:38 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterOutlet} from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,45 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'TyroServ-WebSite-FontEnd';
+
+  constructor(
+    private router: Router,
+    // private authService: AuthService,
+    private cookieService: CookieService,
+    // private ipService: IpService
+  ) {
+    const cookieToken:string = this.cookieService.get('tokenGamenium');
+    const cookieUser:string = this.cookieService.get('userGamenium');
+
+    if (cookieToken && cookieUser){
+      this.loginWithCookie(cookieToken, cookieUser);
+    }
+  }
+
+
+
+  /******************************************************************************************************************
+   *
+   * VARIABLE GLOBAL
+   *
+   * ******************************************************************************************************************/
+
+  AppEnv: string = "DEV"; // DEV or PROD
+  urlApiDev: string = "http://localhost:8000";
+  urlApiProd: string = "";
+  urlIp:string = "https://tyrolium.fr/Contenu/Php/ip.php?api=json"
+  Debug:Boolean = true; // Active la view Serv and Local
+  isLoggedIn: boolean = false;
+  token: string|any;
+  userConnected: /*UserInterface|*/any;
+  currentDate: Date = new Date();
+
+
+
+
+
+
+
+
+
 }
