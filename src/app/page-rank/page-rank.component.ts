a96cb90916bada353218c20db6f87a7fdd23fb82
TheMaximeSan <mxm.tournier@gmail.com>
2024-09-12 14:44:44 +0200

    update

--- src/app/page-rank/page-rank.component.ts
+++ src/app/page-rank/page-rank.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {RejoinComponent} from "../page-home/rejoin/rejoin.component";
+import {SupportComponent} from "../page-home/support/support.component";
 
 @Component({
   selector: 'app-page-rank',
   standalone: true,
-  imports: [],
+  imports: [
+    RejoinComponent,
+    SupportComponent
+  ],
   templateUrl: './page-rank.component.html',
   styleUrl: './page-rank.component.css'
 })
--- src/app/page-support/page-support.component.ts
+++ src/app/page-support/page-support.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {RejoinComponent} from "../page-home/rejoin/rejoin.component";
+import {HeaderComponent} from "../page-home/header/header.component";
 
 @Component({
   selector: 'app-page-support',
   standalone: true,
-  imports: [],
+  imports: [
+    RejoinComponent,
+    HeaderComponent
+  ],
   templateUrl: './page-support.component.html',
   styleUrl: './page-support.component.css'
 })
