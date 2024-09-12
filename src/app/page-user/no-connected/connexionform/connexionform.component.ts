2ae4f9c2457ac47dc0be99f1b6552c160a23285f
TheMaximeSan <mxm.tournier@gmail.com>
2024-09-12 22:33:18 +0200

    update form

--- src/app/page-user/no-connected/connexionform/connexionform.component.ts
+++ src/app/page-user/no-connected/connexionform/connexionform.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-connexionform',
   standalone: true,
-  imports: [],
+    imports: [
+        RouterLink
+    ],
   templateUrl: './connexionform.component.html',
   styleUrl: './connexionform.component.css'
 })
