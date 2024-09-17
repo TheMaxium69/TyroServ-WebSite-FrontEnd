d4697b88c3b4240340bbda1f9acecc93394692cf
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2024-09-17 09:43:04 +0200

    fixe erreur fichier

--- src/app/page-home/faction/faction.component.ts
+++ src/app/page-home/faction/faction.component.ts
@@ -1,36 +1,14 @@
-import { NgFor } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { ApiReponseInterface } from '../../_interface/api-reponse.interface';
-import { RankPlayerS1Interface } from '../../_interface/rank-interface/rank-player-s1.interface';
-import { RankService } from '../../_service/rank/rank.service';
-import { AppComponent } from '../../app.component';
+
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-faction',
   standalone: true,
-  imports: [RouterLink, NgFor],
+  imports: [],
 templateUrl: './faction.component.html',
   styleUrl: './faction.component.css'
 })
-export class FactionComponent implements OnInit {
-  leaderboard: RankPlayerS1Interface[]|undefined;
-
-  constructor(private rankService: RankService,
-              private app:AppComponent) { }
-
-  ngOnInit() {
-
-    this.rankService.getRankS1PlayerPreview(this.app.setURL()).subscribe((response: ApiReponseInterface) => {
-
-      if (response.why == "Succes Request" && Array.isArray(response.data)) {
-        this.leaderboard = response.data;
-        console.log(this.leaderboard);
-      }
-
-    });
-
-
-
-  }
+export class FactionComponent {
+  
 }
--- src/app/page-home/leader/leader.component.ts
+++ src/app/page-home/leader/leader.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiReponseInterface } from '../../_interface/api-reponse.interface';
+import { RankPlayerS1Interface } from '../../_interface/rank-interface/rank-player-s1.interface';
+import { RankService } from '../../_service/rank/rank.service';
+import { AppComponent } from '../../app.component';
+import { RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-leader',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgFor],
   templateUrl: './leader.component.html',
   styleUrl: './leader.component.css'
 })
-export class LeaderComponent {
+export class LeaderComponent implements OnInit {
+  leaderboard: RankPlayerS1Interface[]|undefined;
 
+  constructor(private rankService: RankService,
+              private app:AppComponent) { }
+
+  ngOnInit() {
+
+    this.rankService.getRankS1PlayerPreview(this.app.setURL()).subscribe((response: ApiReponseInterface) => {
+
+      if (response.why == "Succes Request" && Array.isArray(response.data)) {
+        this.leaderboard = response.data;
+        console.log(this.leaderboard);
+      }
+
+    });
+
+
+
+  }
 }
