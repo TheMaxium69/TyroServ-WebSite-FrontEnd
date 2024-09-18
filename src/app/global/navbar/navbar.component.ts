4095d02f9c195c1c592e510c9e75996336725557
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2024-09-18 16:36:36 +0200

    add responsive navbar

--- src/app/global/navbar/navbar.component.ts
+++ src/app/global/navbar/navbar.component.ts
@@ -2,17 +2,21 @@ import {Component, OnInit} from '@angular/core';
 import {NavigationEnd, Router, RouterLink} from "@angular/router";
 import { HideService } from '../../_service/hide/hide.service';
 import {AppComponent} from "../../app.component";
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgClass
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isMenuOpen:boolean = false;
+
 
   constructor(public hideService: HideService,
               private router: Router,
@@ -26,6 +30,10 @@ export class NavbarComponent {
 
   logout() {
     this.app.loggout();
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 
 }
