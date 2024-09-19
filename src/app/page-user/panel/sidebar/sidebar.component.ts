5a995124b18e0a88de7f3d2813908ebf0080813d
TheMaximeSan <mxm.tournier@gmail.com>
2024-09-19 14:24:57 +0200

    update

--- src/app/page-user/panel/sidebar/sidebar.component.ts
+++ src/app/page-user/panel/sidebar/sidebar.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { AppComponent } from '../../../app.component';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
