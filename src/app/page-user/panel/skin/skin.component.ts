cf4b50bb5b33d4538749f2084dc23eff7c29424b
TheMaximeSan <mxm.tournier@gmail.com>
2024-09-22 00:56:50 +0200

    update skin

--- src/app/page-user/panel/skin/skin.component.ts
+++ src/app/page-user/panel/skin/skin.component.ts
@@ -9,5 +9,26 @@ import { AppComponent } from '../../../app.component';
   styleUrl: './skin.component.css'
 })
 export class SkinComponent {
+
+  previewUrl:string | ArrayBuffer | null = 'no-skin.png';
+
   constructor(protected app: AppComponent) { }
+
+  onFileSelected(event: Event) {
+    const fileInput = event.target as HTMLInputElement;
+    if (fileInput?.files && fileInput.files[0]) {
+      const file = fileInput.files[0];
+      if (file.type === 'image/png') {
+        const reader = new FileReader();
+        reader.onload = () => {
+          this.previewUrl = reader.result;
+        };
+        reader.readAsDataURL(file);
+      } else {
+        alert('Please select a PNG image.');
+      }
+    }
+  }
+
+
 }
