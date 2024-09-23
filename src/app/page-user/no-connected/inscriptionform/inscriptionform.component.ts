52c2dc1d7b056354fd71b62aa0d169198173593b
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2024-09-23 10:03:50 +0200

    update inscription

--- src/app/page-user/no-connected/inscriptionform/inscriptionform.component.ts
+++ src/app/page-user/no-connected/inscriptionform/inscriptionform.component.ts
@@ -3,22 +3,44 @@ import { Location } from '@angular/common';
 import {RouterLink} from "@angular/router";
 import {FaviconService} from "../../../_service/favicon/favicon.service";
 import {AppComponent} from "../../../app.component";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-inscriptionform',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule,
   ],
   templateUrl: './inscriptionform.component.html',
   styleUrl: './inscriptionform.component.css'
 })
 export class InscriptionformComponent implements OnInit {
 
+  public formInscription: FormGroup = new FormGroup({
+    pseudo: new FormGroup({
+      pseudo: new FormControl('', [Validators.required]),
+    }),
+    email: new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+    }),
+    password: new FormGroup({
+      password: new FormControl('', [Validators.required]),
+    }),
+    confirmCondition: new FormGroup({
+      confirmCondition: new FormControl('', [Validators.required]),
+    }),
+  });
+
   constructor(private faviconService: FaviconService) { }
 
   ngOnInit() {
     this.faviconService.setTitle('Inscription - TyroServ');
+  }
+
+
+  Inscription(){
+    
   }
 
 }
